d27f257ead0a7b9faabbd336156a464d8f11f912
joshhhwashere <joshua.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-05-07 13:58:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const DUMMY_TEAMS = [
+    {
+        name: 'Barcelona',
+        players: ['Messi', 'Neymar', 'Suarez']
+    },
+    {
+        name: 'Real Madrid',
+        players: ['Ramos', 'Benzema', 'Cristiano']
+    },
+    {
+        name: 'Manchester United',
+        players: ['Rashford', 'Fernandes', 'Casemiro']
+    }
+];
+
+app.get('/api/teams', (req, res, next) => {
+    const teams = DUMMY_TEAMS.map(team => team);
+    res.send(teams);
+    next();
+});
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
